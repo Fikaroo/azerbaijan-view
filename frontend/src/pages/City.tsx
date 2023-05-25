e714816ecb46d0aea5847368f5ed26596e7e4e23
Fuad <fuadbayramv@gmail.com>
2023-05-25 16:22:50 +0400

    country

--- frontend/src/pages/City.tsx
+++ frontend/src/pages/City.tsx
@@ -1,7 +1,8 @@
 import { request } from "graphql-request";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useSWR from "swr";
 import { Kodlar, Rayon, Seher } from "../../types/types";
+import { ArrowLeftCircleIcon } from "@heroicons/react/24/solid";
 
 const fetcher = (
   query: string,
@@ -57,32 +58,132 @@ const City = () => {
   const city = citydata?.Seher; // Seher datasi
   const village = citydata?.Rayon; // Rayon datasi
 
+  const details = {
+    "Əhali sayı": city.Ehali,
+    Ərazi: city.Erazi,
+    "Rayon sayı": city.RayonSayi,
+    "Poçt indeksi": city.PoctIndeksi,
+    "Orta aylıq temperatur": city.OrtaAyliqTemperatur,
+    "Yağıntının miqdarı": city.YagintininMiqdari,
+  };
+
+  const villageDetails = village?.map(
+    ({
+      RayonAdi,
+      Ehali,
+      Erazi,
+      BelediyyelerinSayi,
+      RayonIcraHakimiyyetininTelefonu,
+      PoctIndeksi,
+      KendlerinSayi,
+    }) => ({
+      "Rayon Adi": RayonAdi,
+      "Bələdiyyələrin Sayi": BelediyyelerinSayi,
+      Əhali: Ehali,
+      Ərazi: Erazi,
+      "Kəndlərin Sayi": KendlerinSayi,
+      "Poçt İndeksi": PoctIndeksi,
+      "Rayon Icra Hakimiyyətinin Telefonu": RayonIcraHakimiyyetininTelefonu,
+    })
+  );
+
   return (
-    <div className="w-full py-10">
-      <div className="flex items-center justify-between">
-        <div className="flex-col justify-center text-center">
-          <h1 className="text-3xl">Baki</h1>
-          <div className="flex flex-col text-left ">
-            <span>Əhali sayı: {city.Ehali}</span>
-            <span>Ərazi: {city.Erazi}</span>
-            <span>Rayon sayı: {city.RayonSayi}</span>
-            <span>Poçt indeksi: {city.PoctIndeksi}</span>
-            <span>Orta aylıq temperatur: {city.OrtaAyliqTemperatur}</span>
-            <span>Yağıntının miqdarı: {city.YagintininMiqdari}</span>
+    <div className="w-full py-10 mb-10">
+      <Link to={"/"} className="flex gap-4 mb-4 btn w-fit">
+        <ArrowLeftCircleIcon className="w-8 h-8" /> <span>{city.SeherAdi}</span>
+      </Link>
+      <div>
+        <div className="w-full h-96 carousel">
+          <div id="slide1" className="relative w-full carousel-item">
+            <img
+              className="object-cover w-full h-full rounded-lg"
+              src="https://images.unsplash.com/photo-1596306499398-8d88944a5ec4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1712&q=80"
+              alt="baku"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="relative w-full carousel-item">
+            <img
+              className="object-cover w-full h-full rounded-lg"
+              src="https://images.unsplash.com/photo-1622673486788-9e20bd2e8a1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFraXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=80"
+              alt="baku"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
         </div>
-        <div className="bg-gray-200 shadow w-96 h-96">
-          <img
-            className="object-cover w-full h-full rounded-lg"
-            src="https://images.unsplash.com/photo-1596306499398-8d88944a5ec4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1712&q=80"
-            alt="baku"
-          />
+        <div className="flex flex-col justify-center gap-4 py-6">
+          <h1 className="text-3xl font-semibold">{city.SeherAdi}</h1>
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  {Object.keys(details).map((key) => (
+                    <th>{key}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <td>{citydata.SeherKod}</td>
+                  {Object.values(details).map((val) => (
+                    <td>{val}</td>
+                  ))}
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-      {/* Rayonlar haqqinda melumatlar */}
-      {village.map(({ RayonAdi, RayonIcraHakimiyyetininTelefonu }) => (
-        <div>{RayonAdi}</div>
-      ))}
+
+      <div>
+        <h2 className="text-xl font-medium">
+          {city.SeherAdi} Rayonları : {city.RayonSayi}
+        </h2>
+        {/* Rayonlar haqqinda melumatlar */}
+        <div className="grid h-full grid-cols-3 py-4">
+          {villageDetails.map((detail) => (
+            <div className="h-full rounded-b">
+              <div className="flex items-center justify-center w-full h-12 rounded-t bg-gradient-to-r from-sky-600 to-blue-600">
+                <h3 className="text-2xl font-semibold text-gray-50 ">
+                  {detail["Rayon Adi"]}
+                </h3>
+              </div>
+
+              <div className="grid h-full py-4 bg-gray-200">
+                {Object.entries(detail).map(([key, value], index) => (
+                  <>
+                    {index !== 0 ? (
+                      <div className="flex flex-wrap items-center gap-4">
+                        <span className="block w-1/2 btn btn-ghost">{key}</span>{" "}
+                        <span className="block">{value}</span>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
